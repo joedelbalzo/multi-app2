1294707ae55e1e150b971acf1ad201c00bfbcb7a
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-09-05 19:07:06 -0400

    v0.0.1

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: { babelrc: true },
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
